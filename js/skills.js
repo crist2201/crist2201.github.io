19b206fc5a839065c5acfe6335cc40b5f627fd8b
crist2201 <cristiansalazar822@gmail.com>
2025-10-03 18:42:15 -0400

    change structure

--- js/skills.js
+++ js/skills.js
@@ -5,86 +5,86 @@ document.addEventListener('DOMContentLoaded', () => {
             "category_icon": "fas fa-code",
             "category_name": "Programming Languages",
             "skills": [
-                {"name": "Python", "level": "Proficient","logo": "../static/images/python.png"},
-                {"name": "SQL", "level": "Proficient", "logo": "../static/images/sql.png"},
-                {"name": "C/C++", "level": "Intermediate", "logo": "../static/images/c.png"},
-                {"name": "CSS", "level": "Intermediate", "logo": "../static/images/css.png"},
-                {"name": "HTML", "level": "Intermediate", "logo": "../static/images/html.png"},
-                {"name": "JavaScript/TypeScript", "level": "Beginner", "logo": "../static/images/ts.png"}
+                {"name": "Python", "level": "Proficient","logo": "static/images/python.png"},
+                {"name": "SQL", "level": "Proficient", "logo": "static/images/sql.png"},
+                {"name": "C/C++", "level": "Intermediate", "logo": "static/images/c.png"},
+                {"name": "CSS", "level": "Intermediate", "logo": "static/images/css.png"},
+                {"name": "HTML", "level": "Intermediate", "logo": "static/images/html.png"},
+                {"name": "JavaScript/TypeScript", "level": "Beginner", "logo": "static/images/ts.png"}
             ]
         },
         {
             "category_icon": "fas fa-cogs",
             "category_name": "Machine Learning and AI",
             "skills": [
-                {"name": "TensorFlow", "logo": "../static/images/tf.png"},
-                {"name": "PyTorch", "logo": "../static/images/pytorch.png"},
-                {"name": "Keras", "logo": "../static/images/keras.png"},
-                {"name": "Scikit-learn", "logo": "../static/images/scikit.png"},
-                {"name": "OpenCV", "logo": "../static/images/opencv.png"},
-                {"name": "Hugging Face Transformers", "logo": "../static/images/huggingface.png"}
+                {"name": "TensorFlow", "logo": "static/images/tf.png"},
+                {"name": "PyTorch", "logo": "static/images/pytorch.png"},
+                {"name": "Keras", "logo": "static/images/keras.png"},
+                {"name": "Scikit-learn", "logo": "static/images/scikit.png"},
+                {"name": "OpenCV", "logo": "static/images/opencv.png"},
+                {"name": "Hugging Face Transformers", "logo": "static/images/huggingface.png"}
             ]
         },
         {
             "category_icon": "fas fa-coins",
             "category_name": "Databases and Data Engineering ",
             "skills": [
-                {"name": "MySQL", "logo": "../static/images/mysql.png"},
-                {"name": "MongoDB", "logo": "../static/images/mongodb.png"},
-                {"name": "Pandas", "logo": "../static/images/pandas.png"},
-                {"name": "Numpy", "logo": "../static/images/numpy.png"},
-                {"name": "Spark", "logo": "../static/images/spark.png"},
-                {"name": "Data modelling", "logo": "../static/images/modelling.png"}
+                {"name": "MySQL", "logo": "static/images/mysql.png"},
+                {"name": "MongoDB", "logo": "static/images/mongodb.png"},
+                {"name": "Pandas", "logo": "static/images/pandas.png"},
+                {"name": "Numpy", "logo": "static/images/numpy.png"},
+                {"name": "Spark", "logo": "static/images/spark.png"},
+                {"name": "Data modelling", "logo": "static/images/modelling.png"}
             ]
         },
         {
             "category_icon": "fas fa-chart-line",
             "category_name": "BI and Data Visualization",
             "skills": [
-                {"name": "Matplotlib", "logo": "../static/images/matplot.png"},
-                {"name": "Seaborn", "logo": "../static/images/seaborn.png"},
-                {"name": "Power BI", "logo": "../static/images/powerbi.png"},
-                {"name": "Streamlit", "logo": "../static/images/streamlit.png"}
+                {"name": "Matplotlib", "logo": "static/images/matplot.png"},
+                {"name": "Seaborn", "logo": "static/images/seaborn.png"},
+                {"name": "Power BI", "logo": "static/images/powerbi.png"},
+                {"name": "Streamlit", "logo": "static/images/streamlit.png"}
             ]
         },
         {
             "category_icon": "fas fa-cloud-arrow-up",
             "category_name": "Cloud Computing",
             "skills": [
-                {"name": "AWS", "logo": "../static/images/aws.png"},
-                {"name": "Google Cloud", "logo": "../static/images/gcp.png"},
-                {"name": "Docker", "logo": "../static/images/docker.png"}
+                {"name": "AWS", "logo": "static/images/aws.png"},
+                {"name": "Google Cloud", "logo": "static/images/gcp.png"},
+                {"name": "Docker", "logo": "static/images/docker.png"}
             ]
         },
         {
             "category_icon": "fas fa-check-to-slot",
             "category_name": "QA Automation and Testing",
             "skills": [
-                {"name": "Selenium", "logo": "../static/images/selenium.png"},
-                {"name": "Playwright", "logo": "../static/images/playwright.png"},
-                {"name": "Robot Framework", "logo": "../static/images/robot.png"},
-                {"name": "Postman", "logo": "../static/images/postman.png"},
-                {"name": "Swagger", "logo": "../static/images/swagger.png"}
+                {"name": "Selenium", "logo": "static/images/selenium.png"},
+                {"name": "Playwright", "logo": "static/images/playwright.png"},
+                {"name": "Robot Framework", "logo": "static/images/robot.png"},
+                {"name": "Postman", "logo": "static/images/postman.png"},
+                {"name": "Swagger", "logo": "static/images/swagger.png"}
             ]
         },
         {
             "category_icon": "fas fa-microchip",
             "category_name": "Electronic Engineering",
             "skills": [
-                {"name": "Embedded Software Development", "logo": "../static/images/embedded.png"},
-                {"name": "Power Electronics", "logo": "../static/images/power.png"},
-                {"name": "Circuit Design", "logo": "../static/images/circuit.png"}
+                {"name": "Embedded Software Development", "logo": "static/images/embedded.png"},
+                {"name": "Power Electronics", "logo": "static/images/power.png"},
+                {"name": "Circuit Design", "logo": "static/images/circuit.png"}
             ]
         },
         {
             "category_icon": "fas fa-users",
             "category_name": "Collaboration and Project Management",
             "skills": [
-                {"name": "Agile Methodologies", "logo": "../static/images/scrum.png"},
-                {"name": "GIT", "logo": "../static/images/git.png"},
-                {"name": "JIRA", "logo": "../static/images/jira.png"},
-                {"name": "Notion", "logo": "../static/images/notion.png"},
-                {"name": "CI/CD", "logo": "../static/images/jenkins.png"}
+                {"name": "Agile Methodologies", "logo": "static/images/scrum.png"},
+                {"name": "GIT", "logo": "static/images/git.png"},
+                {"name": "JIRA", "logo": "static/images/jira.png"},
+                {"name": "Notion", "logo": "static/images/notion.png"},
+                {"name": "CI/CD", "logo": "static/images/jenkins.png"}
             ]
         },
         {
